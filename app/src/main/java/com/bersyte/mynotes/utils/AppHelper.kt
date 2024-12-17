d54547ff8dfb4619c03cbac564211e35f9ea2cc0
isaiascuvula <bersyteinfo@gmail.com>
2024-12-17 23:42:20 +0200

    generateColor

--- app/src/main/java/com/bersyte/mynotes/utils/AppHelper.kt
+++ app/src/main/java/com/bersyte/mynotes/utils/AppHelper.kt
@@ -1,9 +1,13 @@
 package com.bersyte.mynotes.utils
-
+import android.graphics.Color
+import kotlin.random.Random
 
 class AppHelper {
 
-    companion object{
-
+    fun generateColor(): Int{
+        val red = Random.nextInt(0, 256)
+        val green = Random.nextInt(0, 256)
+        val blue = Random.nextInt(0, 256)
+        return Color.rgb(red, green, blue)
     }
 }
