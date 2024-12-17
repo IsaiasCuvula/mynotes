047ea41efc03a0b6a21fb0e1b09ee6072d094b70
isaiascuvula <bersyteinfo@gmail.com>
2024-12-17 23:42:38 +0200

    show toast

--- app/src/main/java/com/bersyte/mynotes/features/notes/views/AddNote.kt
+++ app/src/main/java/com/bersyte/mynotes/features/notes/views/AddNote.kt
@@ -1,27 +1,22 @@
 package com.bersyte.mynotes.features.notes.views
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,7 +24,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.bersyte.mynotes.common.components.CommonTextField
@@ -42,38 +37,45 @@ fun AddNote(
     var title by remember { mutableStateOf("") }
     var note by remember { mutableStateOf("") }
     val colorScheme = MaterialTheme.colorScheme
+    val context = LocalContext.current
 
     Scaffold(
-       topBar = {
-           CenterAlignedTopAppBar(
-               title = { Text("Create Note") },
-               navigationIcon = {
-                   IconButton(
-                       onClick = navigateBack
-                   ) {
-                       Icon(
-                           imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                           contentDescription = "Navigate Back button",
-                           tint = colorScheme.background
-                       )
-                   }
-               },
-               colors = TopAppBarDefaults.topAppBarColors(
-                   containerColor = colorScheme.primary,
-                   titleContentColor = colorScheme.background,
-               ),
-               actions = {
-                   TextButton(
-                       onClick = {}
-                   ) {
-                       Text(
-                           "Save",
-                           color = colorScheme.background
-                       )
-                   }
-               }
-           )
-       }
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Create note") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = navigateBack
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Navigate Back button",
+                            tint = colorScheme.background
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorScheme.primary,
+                    titleContentColor = colorScheme.background,
+                ),
+                actions = {
+                    TextButton(
+                        onClick = {
+                            Toast.makeText(
+                                context,
+                                "Note created successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    ) {
+                        Text(
+                            "Save",
+                            color = colorScheme.background
+                        )
+                    }
+                }
+            )
+        }
 
     ) { innerPadding ->
 
@@ -91,7 +93,7 @@ fun AddNote(
             ) {
                 CommonTextField(
                     value = title,
-                    onValueChange = {value ->
+                    onValueChange = { value ->
                         title = value
                     },
                     placeholder = {
@@ -119,5 +121,4 @@ fun AddNote(
             }
         }
     }
-
 }
