bd0a7d3e3733af84a9c1c7a82e5494e9b2dd6697
isaiascuvula <bersyteinfo@gmail.com>
2024-12-18 01:02:14 +0200

    Note Entity done

--- app/src/main/java/com/bersyte/mynotes/features/notes/data/models/Note.kt
+++ app/src/main/java/com/bersyte/mynotes/features/notes/data/models/Note.kt
@@ -1,8 +1,15 @@
 package com.bersyte.mynotes.features.notes.data.models
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.datetime.LocalDateTime
+
+@Entity(tableName = "notes")
 data class Note(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title: String,
     val note: String,
-    val color: Int
+    val color: Int,
+    val createdAt: LocalDateTime
 )
