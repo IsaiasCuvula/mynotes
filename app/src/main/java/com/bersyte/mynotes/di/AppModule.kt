d8f28ced6281f4fa62d59eba001d9b6d6dab4739
isaiascuvula <bersyteinfo@gmail.com>
2024-12-19 22:53:02 +0200

    injecting NoteDatabase

--- app/src/main/java/com/bersyte/mynotes/di/AppModule.kt
+++ app/src/main/java/com/bersyte/mynotes/di/AppModule.kt
@@ -1,10 +1,13 @@
 package com.bersyte.mynotes.di
 
+import android.content.Context
+import androidx.room.Room
 import com.bersyte.mynotes.features.notes.data.datasource.NoteDatabase
 import com.bersyte.mynotes.features.notes.data.repositories.NoteRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -12,6 +15,15 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(
+        @ApplicationContext context: Context
+    ): NoteDatabase = Room.databaseBuilder(
+        context, NoteDatabase::class.java,
+        "note_db"
+    ).build()
 
     @Provides
     @Singleton
