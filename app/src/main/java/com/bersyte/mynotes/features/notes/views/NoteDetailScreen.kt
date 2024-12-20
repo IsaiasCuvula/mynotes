c4a0212d40ebcbdc1d2a4e42e5c6107d3063c598
isaiascuvula <bersyteinfo@gmail.com>
2024-12-21 01:02:03 +0200

    border color and created date while editing

--- app/src/main/java/com/bersyte/mynotes/features/notes/views/NoteDetailScreen.kt
+++ app/src/main/java/com/bersyte/mynotes/features/notes/views/NoteDetailScreen.kt
@@ -2,10 +2,15 @@ package com.bersyte.mynotes.features.notes.views
 
 import android.widget.Toast
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -33,6 +38,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -141,9 +147,12 @@ fun NoteDetailScreen(
             modifier = Modifier.fillMaxSize()
                 .padding(innerPadding)
                 .padding(16.dp)
-                .background(
-                    color = colorScheme.onPrimary,
-                    shape = RoundedCornerShape(16.dp)
+                .background(color = colorScheme.onPrimary)
+                .border(
+                    width = 2.dp,
+                    color = Color(
+                        noteValue.data?.color ?: colorScheme.primary.value.toInt()
+                    )
                 )
         ) {
             when {
@@ -160,9 +169,20 @@ fun NoteDetailScreen(
                     Text("Something went wrong \n${noteState.value.error}" )
                 }
                 noteValue.data != null -> {
+                    val note = noteValue.data
+
                     Column(
                         modifier = Modifier.fillMaxSize()
                     ) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth()
+                                .background(color = Color(note.color))
+                                .padding(horizontal = 16.dp),
+                            horizontalArrangement = Arrangement.End
+                        ) {
+                            Text(note.createdAt.date.toString())
+                        }
+                        Spacer(modifier = Modifier.height(6.dp))
                         CommonTextField(
                             value = title,
                             textStyle = MaterialTheme.typography.titleLarge.copy(
