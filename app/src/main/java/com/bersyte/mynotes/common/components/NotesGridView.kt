e193c6a85351b8b9246ba4b829bebffc66febcbd
isaiascuvula <bersyteinfo@gmail.com>
2024-12-20 19:07:42 +0200

    Centering NoteEmptyCard

--- app/src/main/java/com/bersyte/mynotes/common/components/NotesGridView.kt
+++ app/src/main/java/com/bersyte/mynotes/common/components/NotesGridView.kt
@@ -1,11 +1,13 @@
 package com.bersyte.mynotes.common.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -16,7 +18,13 @@ import com.bersyte.mynotes.features.notes.data.models.Note
 fun NotesGridView(notes: List<Note>, navController: NavController,) {
 
     if(notes.isEmpty()){
-        NoteEmptyCard()
+       Column(
+           modifier = Modifier.fillMaxSize(),
+           verticalArrangement = Arrangement.Center,
+           horizontalAlignment = Alignment.CenterHorizontally
+       ) {
+           NoteEmptyCard()
+       }
     }else {
         LazyVerticalStaggeredGrid(
             verticalItemSpacing = 4.dp,
